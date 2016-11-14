998b530820cbbecd8dd407cf1df180c3d52c7145
Ananda kumar <nlanandkumar@gmail.com>
2016-11-14 23:15:04 +0530

    Update Hello.java

--- src/org/kity/Hello.java
+++ src/org/kity/Hello.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Hello
  */
-@WebServlet("/")
+
 public class Hello extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        

421b70be69271e31e88ae12a5d6a8f03497871fc
anand10717 <anandakumar.n@zohocorp.com>
2016-11-16 02:19:32 +0530

    added db

--- src/org/kity/Hello.java
+++ src/org/kity/Hello.java
@@ -1,16 +1,16 @@
 package org.kity;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.kity.dbconnection.DbConf;
 
 
 public class Hello extends HttpServlet {
@@ -19,55 +19,39 @@ public class Hello extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String name = "";
 		name = request.getParameter("name");
-		
 		try {
+			Statement stmt = DbConf.getInstance().createStatement();
+	         String sql = "CREATE TABLE COMPANY " +
+	                      "(ID INT PRIMARY KEY     NOT NULL," +
+	                      " NAME           TEXT    NOT NULL, " +
+	                      " AGE            INT     NOT NULL, " +
+	                      " ADDRESS        CHAR(50), " +
+	                      " SALARY         REAL)";
+	         stmt.executeUpdate(sql);
+	        
+	         
+	          sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+	                 + "VALUES (1, 'Paul', 32, 'California', 20000.00 );";
+	          stmt.executeUpdate(sql);
+	          ResultSet rs = stmt.executeQuery( "SELECT * FROM COMPANY;" );
+	          while ( rs.next() ) {
+	             int id = rs.getInt("id");
+	               name = rs.getString("name");
+	             int age  = rs.getInt("age");
+	             String  address = rs.getString("address");
+	             float salary = rs.getFloat("salary");
+	             System.out.println( "ID = " + id );
+	             System.out.println( "NAME = " + name );
+	             System.out.println( "AGE = " + age );
+	             System.out.println( "ADDRESS = " + address );
+	             System.out.println( "SALARY = " + salary );
+	             System.out.println();
+	          }
+	          rs.close();
+	          stmt.close();
 
-			Class.forName("org.postgresql.Driver");
-
-		} catch (ClassNotFoundException e) {
-
-			System.out.println("Where is your PostgreSQL JDBC Driver? "
-					+ "Include in your library path!");
-			e.printStackTrace();
-			return;
-
-		}
-
-		System.out.println("PostgreSQL JDBC Driver Registered!");
-
-		Connection connection = null;
-
-		try {
-			
-		URI dbUri = new URI("postgres://tkmliwaapruamw:XF2bN8pRx1c7bGrWmU5ZHVxPzf@ec2-54-75-230-140.eu-west-1.compute.amazonaws.com:5432/d9hg25jjkrk9ec");
-
-	    String username = dbUri.getUserInfo().split(":")[0];
-	    String password = dbUri.getUserInfo().split(":")[1];
-	    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-
-
-			connection = DriverManager.getConnection(
-					dbUrl,username,password);
-
-		} catch (SQLException e) {	
-
-			System.out.println("Connection Failed! Check output console");
-			e.printStackTrace();
-			return;
-
-		} catch (URISyntaxException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		if (connection != null) {
-			System.out.println("You made it, take control your database now!");
-		} else {
-			System.out.println("Failed to make connection!");
-		}
-	
-		try {
-			connection.close();
+	       //  c.close();
+			//DbConf.getInstance().close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
---
+++ src/org/kity/dbconnection/DbConf.java
@@ -0,0 +1,66 @@
+package org.kity.dbconnection;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConf {
+
+	private static Connection connection = null;
+	
+	
+	//private static DbConf conf = null;
+	
+	public static Connection getInstance(){
+		if(connection == null){
+			getDBConn();
+		}
+		return connection;
+	}
+	
+	private DbConf()
+	{
+		
+	}
+	
+	/*private static Connection getConnection() {
+		return connection;
+	}
+
+	private static void setConnection(Connection connection) {
+		DbConf.connection = connection;
+	}*/
+
+	private static void getDBConn(){
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+						e.printStackTrace();
+		}
+
+		try {
+
+			URI dbUri = new URI("postgres://tkmliwaapruamw:XF2bN8pRx1c7bGrWmU5ZHVxPzf@ec2-54-75-230-140.eu-west-1.compute.amazonaws.com:5432/d9hg25jjkrk9ec");
+
+			String username = dbUri.getUserInfo().split(":")[0];
+			String password = dbUri.getUserInfo().split(":")[1];
+			String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+
+			connection = DriverManager.getConnection(dbUrl,username,password);
+		} catch (SQLException e) {	
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+
+		if (connection != null) {
+			System.out.println("You made it, take control your database now!");
+		} else {
+			System.out.println("Failed to make connection!");
+		}
+
+	}
+}
